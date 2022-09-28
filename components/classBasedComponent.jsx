1a0b7435d91ef24a9ee9e84ea1fcdc015b3ebaf9
Lucas Lins <lucaslinsperfil@gmail.com>
2022-09-28 01:28:25 -0300

    test

---
+++ components/classBasedComponent.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+class ClassBasedComponent extends Component{
+  render(){
+    return(
+      'Class base component'
+    )
+  }
+};
+
+export default ClassBasedComponent;
